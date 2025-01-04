45ccd22a405a87d68c019f495145e0746f643b7b
shtmjp <shtm.1058@gmail.com>
2025-01-04 13:31:16 +0900

    resolve build error due to path problem

--- noisy_bnsp/__init__.py
+++ noisy_bnsp/__init__.py
@@ -1,7 +1,5 @@
 """noisy-bnsp: simulate and estimate noisy bivariate Neyman-Scott process."""
 
-__version__ = "0.1.0"
-
 import c_func
 
 from .nsmodels import EstConfig, Estimator, NSExpModel, NSGammaModel
